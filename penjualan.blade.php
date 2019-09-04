56736eecbd569acf042bf4052c4ba7adedf9f3bc
Rashad <46430370+rashadandredi@users.noreply.github.com>
2019-09-04 08:43:14 +0700

    selesai

---
+++ penjualan.blade.php
@@ -0,0 +1,53 @@
+@extends('template')
+@section('content')
+<section class="main-section">
+  <div class="content">
+    <h1>Data Penjualan</h1>
+    @if(Session::has('alert_message'))
+    <div class="alert alert-success">
+      <strong>{{ Session::get('alert_message') }}</strong>
+    </div>
+    @endif
+    <hr>
+    <div>
+      <button class="btn-sm btn-link"><a href="{{url('/barang')}}">Barang</button>
+      <button class="btn-sm btn-link"><a href="{{url('/pembelian')}}">Beli</button>
+      <button class="btn-sm btn-link"><a href="{{url('/penjualan')}}">Jual</button>
+    </div>
+    <table class="table table-bordered">
+       <thead>
+         <tr>
+           <th>ID</th>
+           <th>Kode Barang</th>
+           <th>Jumlah</th>
+           <th>Total Harga</th>
+           <th>Aksi</th>
+         </tr>
+       </thead>
+       <tbody>
+         @php $id = 1; @endphp
+         @foreach($data as $datas)
+         <tr>
+           <td>{{ $id++ }}</td>
+           <td>{{ $datas->kd_barang }}</td>
+           <td>{{ $datas->jumlah }}</td>
+           <td>{{ $datas->total_harga }}</td>
+           <td>
+              <form action="{{ route('penjualan.destroy', $datas->id) }}" method="post">
+                {{ csrf_field() }}
+                {{ method_field('DELETE') }}
+                <a href="{{ route('penjualan.edit',$datas->id) }}" class=" btn btn-sm btn-primary">Edit</a>
+                <button class="btn btn-sm btn-danger" type="submit" onclick="return confirm('Yakin ingin menghapus data?')">Delete</button>
+              </form>
+           </td>
+         </tr>
+         @endforeach
+       </tbody>
+    </table>
+    <button class="btn-sm btn-link"><a href="{{url('/penjualan/create')}}">Buat</button>
+
+  </div>
+
+</section>
+
+@endsection
